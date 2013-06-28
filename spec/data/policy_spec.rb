422d1496cbb8e134dc93ae2dc688eeac222ea7f2
David Lutterkort <lutter@watzmann.net>
2013-06-28 17:29:01 +0200

    Make policy binding respect max_count

--- spec/data/policy_spec.rb
+++ spec/data/policy_spec.rb
@@ -16,6 +16,30 @@ describe Razor::Data::Policy do
     @node.policy.should == pl
   end
 
+  describe "max_count" do
+    it "binds if there is room" do
+      pl = make_policy(:image => @image, :installer_name => "dummy",
+                       :max_count => 1)
+      pl.add_tag(@tag)
+      pl.save
+      Policy.bind(@node)
+      @node.policy.should == pl
+    end
+
+    it "does not bind if there is no room" do
+      pl = make_policy(:image => @image, :installer_name => "dummy",
+                       :max_count => 0)
+      pl.add_tag(@tag)
+      pl.save
+      Policy.bind(@node)
+      @node.policy.should be_nil
+    end
+
+    # It would be nice to test that we do not exceed max_count bindings in
+    # racy situations, but I don't see a good way to do that without
+    # modifying Policy.bind in a way that let's us inject the race reliably
+  end
+
   it "does not bind disabled policy" do
     pl = make_policy(:image => @image, :installer_name => "dummy",
                      :enabled => false)
