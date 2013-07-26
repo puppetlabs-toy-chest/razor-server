221f252b2c4451397161db28d1eb27eaa8122f80
Alex Konradi <alex.konradi@rackspace.com>
2013-07-26 09:21:06 -0500

    Create API package and base transformer class
    Razor::API will contain classes for fielding requests to the API endpoint. The
    classes will wrap persistence objects (stuff in Razor::Data) and provide nice,
    consistent, JSON-able hashes that can be presented to the API consumer.
    
    Razor::API::Transform is a base class for the API translators, though it
    currently only provides a few helper methods like #to_json.

---
+++ lib/razor/api.rb
@@ -0,0 +1,25 @@
+# The classes in Razor::API serve as a wrapper around the persistence layer
+# (Razor::Data) to provide a consistent API to Razor clients. This allows the
+# internal implementation to introduce non-backwards-compatible changes while
+# maintaining external compatability.
+
+module Razor::API
+
+  # Base class for persistent object to JSON API translators. Transformations
+  # can be performed by calling Transform.to_hash(obj) or
+  # Transform.new(obj).to_hash
+  class Transform
+    def self.to_json(obj)
+      self.new(obj).to_json
+    end
+
+    def self.to_hash(obj)
+      self.new(obj).to_hash
+    end
+
+    def to_json
+      to_hash.to_json
+    end
+  end
+end
+
