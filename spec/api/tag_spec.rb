4c7c85025ab40d49f64c2dee77c8fe2d3bf01d35
Alex Konradi <alex.konradi@rackspace.com>
2013-07-29 11:34:51 -0500

    Add unit tests for Razor::API::Tag
    These test ensure that Razor::API::Tag#to_hash and #to_json return the expected
    values, and that the output of the two is the same given that #to_hash's keys
    are symbols and JSON.parse(#to_json) returns string keys. Admittedly, the
    :symbolize_names option could be passed to JSON.parse, but then the strings in
    the rule array would not match since they're symbols.

---
+++ spec/api/tag_spec.rb
@@ -0,0 +1,51 @@
+require_relative '../spec_helper'
+require 'json'
+
+describe Razor::API::Tag do
+
+  before(:each) do
+    @t ||= Razor::Data::Tag.create(:name=>"tag 0",
+                                :rule=>["=", ["fact", "zero"], "0"] )
+    @tag = Razor::API::Tag.new(@t)
+  end
+
+  subject { @tag }
+
+  it "can output hashes" do
+    should respond_to(:to_hash)
+    @tag.to_hash.should be_a(Hash)
+  end
+
+  it "can output json" do
+    should respond_to(:to_json)
+    @tag.to_json.should be_a(String)
+  end
+
+  it "makes JSON that mirrors the hash value" do
+    Hash[ @tag.to_hash.map do |k, v|
+      [k.to_s, v]
+    end ].should == JSON.parse(@tag.to_json)
+  end
+
+  it "has only the specified keys" do
+    expected_keys = [:name, :rule]
+
+    @tag.to_hash.should have(expected_keys.size).keys
+    expected_keys.each do |key|
+      @tag.to_hash.should have_key(key)
+    end
+  end
+
+  describe ":name" do 
+    subject { @tag.to_hash[:name] }
+    
+    it { should be_a String }
+  end
+
+  describe ":rule" do
+    subject { @tag.to_hash[:rule] }
+
+    it { should be_an Array }
+  end
+
+end
