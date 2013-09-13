90dd1ff3e83dabb913e19afda90ff6f8e1770d9b
David Lutterkort <lutter@watzmann.net>
2013-09-12 18:39:33 -0700

    Switch image unpacking over to use bsdtar
    7zip truncates long file names which is an issue for, e.g., the CentOS
    ISO's: http://sourceforge.net/p/sevenzip/bugs/1055/

--- lib/razor/iso.rb
+++ lib/razor/iso.rb
@@ -6,20 +6,20 @@ module Razor::ISO
   #
   # @raises if there is no available unpacker
   def self.unpack(iso, destination)
-    unless system(find_7z, 'x', "-o#{destination}", '-y', '-bd', iso.to_s)
+    unless system(find_bsdtar, 'xf', iso.to_s, '-C', destination.to_s)
       # @todo danielp 2013-07-16: ignore warnings during unpacking; is that
       # the right call?  For now, I think so, since they are 'non-fatal'
       # warnings and to date I have never hit one that was a problem for me.
-      $?.exitstatus > 1 and raise "failed executing 7z: #{$?.inspect}"
+      $?.exitstatus > 1 and raise "failed executing bsdtar: #{$?.inspect}"
     end
     return true
   end
 
-  def self.find_7z
+  def self.find_bsdtar
     ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-      exe = File.join(path, '7z')
+      exe = File.join(path, 'bsdtar')
       return exe if File.executable? exe
     end
-    raise "the 7z unpacker was not found in the path"
+    raise "the bsdtar unpacker was not found in the path"
   end
 end
--- spec/iso_spec.rb
+++ spec/iso_spec.rb
@@ -2,37 +2,38 @@ require 'spec_helper'
 require 'tmpdir'
 
 describe Razor::ISO do
-  describe "find_7z" do
+  describe "find_bsdtar" do
     it "should raise if the command is not found" do
       File.stub(:executable?).and_return false
-      expect { Razor::ISO.find_7z }.
-        to raise_error RuntimeError, /the 7z unpacker was not found/
+      expect { Razor::ISO.find_bsdtar }.
+        to raise_error RuntimeError, /the bsdtar unpacker was not found/
     end
 
     it "should return the path to the executable if it is found" do
       stub_const('ENV', {'PATH' => '/a:/b:/c:/d:/e'})
-      File.stub(:executable?) {|path| path == '/c/7z' }
-      Razor::ISO.find_7z.should == '/c/7z'
+      File.stub(:executable?) {|path| path == '/c/bsdtar' }
+      Razor::ISO.find_bsdtar.should == '/c/bsdtar'
     end
 
     it "should return the first executable on the path" do
       stub_const('ENV', {'PATH' => '/a:/b:/c:/d:/e'})
-      File.stub(:executable?) {|path| path == '/c/7z' or path == '/e/7z' }
-      Razor::ISO.find_7z.should == '/c/7z'
+      File.stub(:executable?) {|path| ['/c/bsdtar', '/e/bsdtar'].include?(path) }
+      Razor::ISO.find_bsdtar.should == '/c/bsdtar'
     end
   end
 
   describe "unpack" do
     before :each do
-      Razor::ISO.find_7z rescue pending "7z is not installed on this machine"
+      Razor::ISO.find_bsdtar rescue pending "bsdtar is not installed on this machine"
     end
 
     let :tiny_iso do Pathname(__FILE__).dirname + 'fixtures' + 'iso' + 'tiny.iso' end
 
-    it "should unpack the image with 7z" do
+    it "should unpack the image with bsdtar" do
       Dir.mktmpdir do |dir|
         Razor::ISO.unpack(tiny_iso, dir)
         File.read(Pathname(dir) + 'content.txt').should == "This is the life!\n"
+        File.read(Pathname(dir) + 'file-with-filename-that-is-longer-than-64-characters-which-some-unpackers-get-wrong.txt').should == "7zip for example\n"
       end
     end
   end
