dd54fe1949adc560d9204ec376452808113e51f8
Simon Fuhrer <simonfuhrer@tecbiz.ch>
2013-11-07 17:03:30 +0100

    add modifications from Daniel to app.rb

---
+++ spec/app/svc_nodeid_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../spec_helper'
+require_relative '../../app'
+
+describe "/svc/nodeid" do
+  include Rack::Test::Methods
+
+  let :app do
+    Razor::App
+  end
+
+  let :node do
+    Fabricate(:node)
+  end
+
+  it "should 400 if no parameters are passed" do
+    get '/svc/nodeid'
+    last_response.status.should == 400
+  end
+
+  it "should return the node ID given the mac address only" do
+    get "/svc/nodeid?net0=#{node.hw_hash["mac"].first}"
+    last_response.status.should == 200
+    last_response.json.should == { 'id' => node.id }
+  end
+end
