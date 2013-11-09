62872251eb89a8dc63b2238225a02603bb8e05ba
Simon Fuhrer <simonfuhrer@tecbiz.ch>
2013-11-09 09:32:15 +0100

    This commit extends the windows installer! The following new features are implemented - Dynamic disovery the Razor server (it is no longer assumed that the DHCP server is identical to the razor server) - Windows 8PRO inherits from the new genric windows model - The Generic Modul installs Windows 2012 R2 Standard Edition - New Windows Puppet broker. (Thanks to chocolatey.org) - SMB / CIFS shares can now be configured in config.yaml - Advanced Logging: Each step will be logged!

--- build-winpe/razor-client.ps1
+++ build-winpe/razor-client.ps1
@@ -1,58 +1,20 @@
-<<<<<<< HEAD
-# -*- powershell -*-
+<#
 
-# If we have a configuration file, source it in.
-$configfile = join-path $env:SYSTEMDRIVE "razor-client-config.ps1"
-if (test-path $configfile) {
-    write-host "sourcing configuration from $configfile"
-    . $configfile
-    # $server is now set
-} else {
-    # No sign of a configuration file, guess that our DHCP server is also our
-    # Razor server, and point at that.  Could easily be wrong, but what else
-    # are we going to do? --daniel 2013-10-28
-    #
-    # @todo danielp 2013-10-29: is there a better way to handle multiple
-    # network adapters with DHCP addresses than I have here?
-    write-host "guessing that DHCP server == Razor server!"
-    $server = get-wmiobject win32_networkadapterconfiguration |
-                  where { $_.ipaddress -and
-                          $_.dhcpenabled -eq "true" -and
-                          $_.dhcpleaseobtained } |
-                  select -uniq -first 1 -expandproperty dhcpserver
+.SYNOPSIS
+second-stage Windows installer script! DHCP Disover, TFTP Download
 
-}
+.DESCRIPTION
+second-stage Windows installer script
 
-$baseurl = "http://${server}:8080/svc"
+.NOTES
+second-stage Windows installer script
 
+.LINK
+https://github.com/puppetlabs/razor-server
 
-# Figure out our node hardware ID details, since we can't get at anything more
-# useful from our boot environment.  Sadly, rediscovery is the order of the
-# day here.  Damn WinPE.
-$hwid = get-wmiobject Win32_NetworkAdapter -filter "netenabled='true'" | `
-            select -expandproperty macaddress | `
-            foreach-object -begin { $n = 0 } -process { $n++; "net${n}=${_}"; }
-$hwid = $hwid -join '&' -replace ':', '-'
+#>
+[cmdletbinding()]param()
 
-# Now, communicate with the server and translate our HWID into a node ID
-# number that we can use for our next step -- accessing our bound
-# installer templates.
-write-host "contact ${baseurl}/nodeid?${hwid} for ID mapping"
-$data = invoke-restmethod "${baseurl}/nodeid?${hwid}"
-$id = $data.id
-write-host "mapped myself to node ID ${id}"
-
-# Finally, fetch down our next stage of script and evaluate it.
-# Apparently this is the best way to just get the string; certainly, it beats
-# the results from `invoke-webrequest` and friends for sanity.
-$url = "${baseurl}/file/${id}/second-stage.ps1"
-write-host "load and execute ${url}"
-(new-object System.Net.WebClient).DownloadString($url) | invoke-expression
-
-# ...and we are done.
-write-host "second stage completed, exiting."
-exit
-=======
 $sourcetftpclient = @'
 using System;
 using System.Text;
@@ -365,7 +327,7 @@ for ($cnt=0; $cnt -le $macaddressesarrays.Length; $cnt++) {
 $dhcp_mac = $net0
 Write-ToConsole "DHCP MAC: " $dhcp_mac
 $uuid = $hwid 
-$RazorDir = "$env:SystemDrive\Razor"
+$RazorDir = "$env:SystemDrive"
 $command1 = "$RazorDir\Net-DhcpDiscover.ps1"
 
 
@@ -412,7 +374,7 @@ if ($dhcpresponse -ne $null){
 	      $webclient.DownloadFile($urlunattened,$localfilename)
 	      Write-ToConsole "Razor-Client downloaded $scriptfilename File, invoke File now: " $localfilename
 	      Invoke-Expression $localfilename
-	    }catch[Exception]{
+	    }catch [Exception]{
 	      write-error $_
 	    }
 
@@ -424,4 +386,3 @@ if ($dhcpresponse -ne $null){
 }
 write-host -foreground green "Deployment Done"
 exit
->>>>>>> 205164f3741caff07d97571b5a58d60bedcf92e2
