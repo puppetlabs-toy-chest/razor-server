6cd5db353cdcad3d78f911203e58355a626193b0
Thomas Trageser <thomast74@gmail.com>
2013-11-10 19:21:19 +0000

    Define architecture as part of the installer
    This adds a manually defined `architecture` field to installers, which
    is useful for the Ubuntu installer -- it generally only ships a single
    architecture on a disk.

---
+++ db/migrate/004_add_installer_architecture.rb
@@ -0,0 +1,17 @@
+require_relative './util'
+
+Sequel.migration do
+  up do
+    extension(:constraint_validations)
+
+    alter_table :installers do
+      add_column            :architecture, :varchar, :size => 16
+    end
+  end
+
+  down do
+    alter_table :installers do
+      drop_column         :architecture
+    end
+  end
+end
