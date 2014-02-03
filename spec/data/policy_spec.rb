39ded00a3f0d60ea48ab37e02719de758b723cd5
Daniel Pittman <daniel@rimspace.net>
2014-02-03 13:02:27 -0800

    Avoid member variables in spec tests
    Member variables in spec tests are a trap: they preserve state in
    unpredictable ways, and can trivially result in state moving between tests
    leading to broken tests that appear to work, or vice-versa.
    
    This replaces the member variables with `let` methods, which provide the same
    stability, but lazily initialize, and ensure that the object is reset
    correctly between tests regardless of other action.
    
    Signed-off-by: Daniel Pittman <daniel@rimspace.net>

--- spec/data/policy_spec.rb
+++ spec/data/policy_spec.rb
@@ -66,15 +66,13 @@ describe Razor::Data::Policy do
   end
 
   context "ordering" do
-    before(:each) do
-      @p1 = Fabricate(:policy, :rule_number => 1)
-      @p2 = Fabricate(:policy, :rule_number => 2)
-    end
+    let :p1 do Fabricate(:policy, :rule_number => 1) end
+    let :p2 do Fabricate(:policy, :rule_number => 2) end
 
     def check_move(where, other, list)
       p  = Fabricate(:policy)
       if where
-        p.move(where, other)
+g        p.move(where, other)
         p.save
       end
 
@@ -83,31 +81,31 @@ describe Razor::Data::Policy do
     end
 
     it "moves existing policy to the end" do
-      @p1.move('after', @p2)
-      Policy.all.map { |p| p.id }.should == [ @p2.id, @p1.id ]
+      p1.move('after', p2)
+      Policy.all.map { |p| p.id }.should == [ p2.id, p1.id ]
     end
 
     it "creates policies at the end of the list" do
-      check_move(nil, nil, [@p1, @p2, :_])
+      check_move(nil, nil, [p1, p2, :_])
     end
 
     describe 'before' do
       it "p1 creates at the head of the table" do
-        check_move('before', @p1, [:_, @p1, @p2])
+        check_move('before', p1, [:_, p1, p2])
       end
 
       it "p2 goes between p1 and p2" do
-        check_move('before', @p2, [@p1, :_, @p2])
+        check_move('before', p2, [p1, :_, p2])
       end
     end
 
     describe "after" do
       it "p1 goes between p1 and p2" do
-        check_move('after', @p1, [@p1, :_, @p2])
+        check_move('after', p1, [p1, :_, p2])
       end
 
       it "p2 goes to the end of the table" do
-        check_move('after', @p2, [@p1, @p2, :_])
+        check_move('after', p2, [p1, p2, :_])
       end
     end
   end
