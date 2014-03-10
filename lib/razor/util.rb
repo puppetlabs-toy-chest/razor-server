d3c77b35f636be1af9fab769fe6b31fde41bde16
Daniel Pittman <daniel@rimspace.net>
2014-03-10 15:05:06 -0700

    Make `json_name_from_ruby` available in Object
    We regularly need to present the user with a name for something they
    submitted, for type checking and so forth.  Since this is so common, and
    happens in so many places, it seems reasonable to have a single central method
    to expose this translation capability to the world.
    
    By design this should take everything -- classes, and instances -- and return
    the correct string for the user.  Returned strings are not translated, since
    these are technical terms, not internal jargon.
    
    https://tickets.puppetlabs.com/browse/RAZOR-118
    
    Signed-off-by: Daniel Pittman <daniel@rimspace.net>

--- lib/razor/util.rb
+++ lib/razor/util.rb
@@ -1,4 +1,23 @@
 # -*- encoding: utf-8 -*-
 module Razor::Util; end
 
+# Make this uniformly available.
+class Object
+  def ruby_type_to_json(ruby)
+    # These should not be translated, since they are technical terms from the
+    # JSON specification.
+    case ruby
+    when Hash    then 'object'
+    when Array   then 'array'
+    when String  then 'string'
+    when Numeric then 'number'
+    when true    then 'true'
+    when false   then 'false'
+    when nil     then 'null'
+    # I don't believe we will ever, ever see this come up, but just in case...
+    else _('unable to translate "%{name}" to JSON type') % {name: ruby.class.name}
+    end
+  end
+end
+
 require_relative 'util/template_config'
