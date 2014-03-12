82c50a83c864b74a68d8bba249c782f9c75f12d2
Daniel Pittman <daniel@rimspace.net>
2014-03-12 16:46:38 -0700

    Use object reference validation for node reinstall commands
    This moves to using the validation DSL to pre-check the node is a valid
    reference when we issue a node-reinstall command.  That avoids the need to
    handle it in the code specifically.
    
    This also changes the return code from 202 to 404 if you issue the command
    against a node that does not exist, which seems reasonable as that is
    ultimately a user intent failure: we can't do what they wanted, and they
    presumably expected *something* to be reinstalled when they issued
    the command.
    
    https://tickets.puppetlabs.com/browse/RAZOR-118
    
    Signed-off-by: Daniel Pittman <daniel@rimspace.net>

--- app.rb
+++ app.rb
@@ -663,32 +663,35 @@ class Razor::App < Sinatra::Base
 
   validate :reinstall_node do
     authz '%{name}'
-    attr  'name', type: String, required: true
+    attr  'name', type: String, required: true, references: Razor::Data::Node
   end
 
   command :reinstall_node do |data|
     actions = []
-    if node = Razor::Data::Node[:name => data['name']]
-      log = { :event => :reinstall }
-      if node.policy
-        log[:policy_name] = node.policy.name
-        node.policy = nil
-        actions << _("node unbound from %{policy}") % {policy: log[:policy_name]}
-      end
-      if node.installed
-        log[:installed] = node.installed
-        node.installed = nil
-        node.installed_at = nil
-        actions << _("installed flag cleared")
-      end
-      if actions.empty?
-        actions << _("no changes; node %{name} was neither bound nor installed") % {name: data['name']}
-      end
-      node.log_append(log)
-      node.save
-    else
-      actions << _("no changes; node %{name} does not exist") % {name: data['name']}
+
+    node = Razor::Data::Node[:name => data['name']]
+    log = { :event => :reinstall }
+
+    if node.policy
+      log[:policy_name] = node.policy.name
+      node.policy = nil
+      actions << _("node unbound from %{policy}") % {policy: log[:policy_name]}
     end
+
+    if node.installed
+      log[:installed] = node.installed
+      node.installed = nil
+      node.installed_at = nil
+      actions << _("installed flag cleared")
+    end
+
+    if actions.empty?
+      actions << _("no changes; node %{name} was neither bound nor installed") % {name: data['name']}
+    end
+
+    node.log_append(log)
+    node.save
+
     # @todo danielp 2014-02-27: I don't know the best way to handle this sort
     # of conjoined string in translation.
     { :result => actions.join(_(" and ")) }
