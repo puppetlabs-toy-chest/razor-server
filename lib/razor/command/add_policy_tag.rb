3f731fbf86be1dba90d51bfbdeb517aa47d4371b
Scott McClellan <scott.mcclellan@puppetlabs.com>
2014-03-27 13:24:43 -0500

    Validate `add_policy_tag` command.
    https://tickets.puppetlabs.com/browse/RAZOR-178

--- lib/razor/command/add_policy_tag.rb
+++ lib/razor/command/add_policy_tag.rb
@@ -1,17 +1,17 @@
 # -*- encoding: utf-8 -*-
 class Razor::Command::AddPolicyTag < Razor::Command
-  attr 'name'
-  attr 'rule'
-  attr 'tag'
+  attr 'name', type: String, required: true, references: Razor::Data::Policy
+  attr 'tag',  type: String, required: true
+  attr 'rule', type: Array
 
   def run(request, data)
-    data['name'] or request.error 400, :error => _("Supply policy name to which the tag is to be added")
-    data['tag'] or request.error 400, :error => _("Supply the name of the tag you which to add")
-
-    policy = Razor::Data::Policy[:name => data['name']] or request.error 404, :error => _("Policy %{name} does not exist") % {name: name}
-    tag = Razor::Data::Tag.find_or_create_with_rule(
-      { 'name' => data['tag'], 'rule' => data['rule'] }
-    ) or request.error 404, :error => _("Tag %{name} does not exist and no rule to create it supplied.") % {name: data['tag']}
+    policy = Razor::Data::Policy[:name => data['name']]
+    begin
+      tag = Razor::Data::Tag.
+        find_or_create_with_rule('name' => data['tag'], 'rule' => data['rule'])
+    rescue ArgumentError => e
+      request.error 422, e.message
+    end
 
     unless policy.tags.include?(tag)
       policy.add_tag(tag)
