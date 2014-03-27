8ec4dbf9edb791569d12a0b70b0535da69c1e387
Scott McClellan <scott.mcclellan@puppetlabs.com>
2014-03-27 14:34:10 -0500

    Validate `update_node_metadata` command.
    https://tickets.puppetlabs.com/browse/RAZOR-178

--- lib/razor/command/update_node_metadata.rb
+++ lib/razor/command/update_node_metadata.rb
@@ -1,31 +1,26 @@
 # -*- encoding: utf-8 -*-
 
 class Razor::Command::UpdateNodeMetadata < Razor::Command
-  attr 'node'
-  attr 'key'
-  attr 'value'
-  attr 'no_replace'
+  attr 'node',       type: String, required: true, references: [Razor::Data::Node, :name]
+  attr 'key',        type: String, exclude: 'all'
+  attr 'all',        type: [String, :bool], exclude: 'key'
+  attr 'value',      required: true
+  attr 'no_replace', type: [String, :bool]
 
+  require_one_of 'key', 'all'
+
+  # Update/add specific metadata key (works with GET)
   def run(request, data)
-    data['node'] or request.error 400,
-      :error => _('must supply node')
-    data['key'] or ( data['all'] and data['all'] == 'true' ) or request.error 400,
-      :error => _('must supply key or set all to true')
-    data['value'] or request.error 400,
-      :error => _('must supply value')
+    # This will get removed when coercion is no longer supported.
+    (!data['no_replace'] or ['true', true].include? data['no_replace']) or
+      request.error 422, :error => _("'no_replace' must be boolean true or string 'true'")
+    (!data['all'] or (['true', true].include? data['all'])) or
+      request.error 422, :error => _("'all' must be boolean true or string 'true'")
 
-    if data['no_replace']
-      data['no_replace'] == true or data['no_replace'] == 'true' or request.error 400,
-        :error => _("no_replace must be boolean true or string 'true'")
-    end
+    node = Razor::Data::Node[:name => data['node']]
+    operation = { 'update' => { data['key'] => data['value'] } }
+    operation['no_replace'] = true unless operation['no_replace'].nil?
 
-    if node = Razor::Data::Node[:name => data['node']]
-      operation = { 'update' => { data['key'] => data['value'] } }
-      operation['no_replace'] = true unless operation['no_replace'].nil?
-
-      node.modify_metadata(operation)
-    else
-      request.error 400, :error => "Node #{data['node']} not found"
-    end
+    node.modify_metadata(operation)
   end
 end
--- spec/app/update_node_metadata_spec.rb
+++ spec/app/update_node_metadata_spec.rb
@@ -7,6 +7,10 @@ describe "update node metadata command" do
 
   let(:app) { Razor::App }
 
+  let(:node) do
+    Fabricate(:node)
+  end
+
   before :each do
     header 'content-type', 'application/json'
     authorize 'fred', 'dead'
@@ -19,22 +23,42 @@ describe "update node metadata command" do
   it "should require a node" do
     data = { 'key' => 'k1', 'value' => 'v1' }
     update_metadata(data)
-    last_response.status.should == 400
-    JSON.parse(last_response.body)["error"].should =~ /must supply node/
+    last_response.status.should == 422
+    last_response.json["error"].should =~ /required attribute node is missing/
   end
 
   it "should require a key" do
-    data = { 'node' => 'node1', 'value' => 'v1' }
+    data = { 'node' => "node#{node.id}", 'value' => 'v1' }
     update_metadata(data)
-    last_response.status.should == 400
-    JSON.parse(last_response.body)["error"].should =~ /must supply key/
+    last_response.status.should == 422
+    last_response.json["error"].should =~ /one of all, key must be supplied/
   end
 
   it "should require a value" do
-    data = { 'node' => 'node1', 'key' => 'k1' }
+    data = { 'node' => "node#{node.id}", 'key' => 'k1' }
     update_metadata(data)
-    last_response.status.should == 400
-    JSON.parse(last_response.body)["error"].should =~ /must supply value/
+    last_response.status.should == 422
+    last_response.json["error"].should =~ /required attribute value is missing/
+  end
+
+  it "should require no_replace to equal true" do
+    data = { 'node' => "node#{node.id}", 'key' => 'k1', 'value' => 'v1', 'no_replace' => 'not true' }
+    update_metadata(data)
+    last_response.status.should == 422
+    last_response.json["error"].should =~ /'no_replace' must be boolean true or string 'true'/
+  end
+
+  it "should require all to equal true" do
+    data = { 'node' => "node#{node.id}", 'value' => 'v1', 'all' => 'not true' }
+    update_metadata(data)
+    last_response.status.should == 422
+    last_response.json["error"].should =~ /'all' must be boolean true or string 'true'/
+  end
+
+  it "should succeed with 'all' and 'no_replace'" do
+    data = { 'node' => "node#{node.id}", 'value' => 'v1', 'all' => 'true', 'no_replace' => 'true' }
+    update_metadata(data)
+    last_response.status.should == 202
   end
 
   #Defer to the modify-node-metadata tests for the verification of the
