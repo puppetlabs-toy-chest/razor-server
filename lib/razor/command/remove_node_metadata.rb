b8942940e93591e3069fc4286401a65f1dafd98c
Scott McClellan <scott.mcclellan@puppetlabs.com>
2014-03-27 14:49:55 -0500

    Validate `remove_node_metadata` command.
    https://tickets.puppetlabs.com/browse/RAZOR-178

--- lib/razor/command/remove_node_metadata.rb
+++ lib/razor/command/remove_node_metadata.rb
@@ -2,26 +2,23 @@
 
 # Remove a specific key or remove all (works with GET)
 class Razor::Command::RemoveNodeMetadata < Razor::Command
-  attr 'node'
-  attr 'key'
-  attr 'value'
-  attr 'all'
+  attr 'node', type: String, required: true, references: [Razor::Data::Node, :name]
+  attr 'key',  type: String
+  attr 'all',  type: [String, :bool]
 
+  require_one_of 'key', 'all'
+
+  # Remove a specific key or remove all (works with GET)
   def run(request, data)
-    data['node'] or request.error 400,
-      :error => _('must supply node')
-    data['key'] or ( data['all'] and data['all'] == 'true' ) or request.error 400,
-      :error => _('must supply key or set all to true')
+    (data['all'] and data['all'] == 'true') or
+      request.error 422, :error => _("invalid value for attribute 'all'")
 
-    if node = Razor::Data::Node[:name => data['node']]
-      if data['key']
-        operation = { 'remove' => [ data['key'] ] }
-      else
-        operation = { 'clear' => true }
-      end
-      node.modify_metadata(operation)
+    node = Razor::Data::Node[:name => data['node']]
+    if data['key']
+      operation = { 'remove' => [ data['key'] ] }
     else
-      request.error 400, :error => _("Node %{name} not found") % {name: data['node']}
+      operation = { 'clear' => true }
     end
+    node.modify_metadata(operation)
   end
 end
