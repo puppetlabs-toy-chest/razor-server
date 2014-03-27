f9133a5336ed687ac1182198a5fc89a3ff0aeafd
Scott McClellan <scott.mcclellan@puppetlabs.com>
2014-03-27 13:35:03 -0500

    Validate `remove_policy_tag` command.
    https://tickets.puppetlabs.com/browse/RAZOR-178

--- app.rb
+++ app.rb
@@ -1007,24 +1007,18 @@ class Razor::App < Sinatra::Base
     end
   end
 
-  command :remove_policy_tag do |data|
-    data['name'] or error 400,
-      :error => _("Supply policy name to which the tag is to be removed")
-    data['tag'] or error 400,
-      :error => _("Supply the name of the tag you which to remove")
+  validate :remove_policy_tag do
+    attr 'name', type: String, required: true, references: Razor::Data::Policy
+    attr 'tag', type: String, required: true
+  end
 
-    policy = Razor::Data::Policy[:name => data['name']] or error 404,
-      :error => _("Policy %{name} does not exist") % {name: data['name']}
+  command :remove_policy_tag do |data|
+    policy = Razor::Data::Policy[:name => data['name']]
     tag = Razor::Data::Tag[:name => data['tag']]
 
-    if tag
-      if policy.tags.include?(tag)
-        policy.remove_tag(tag)
-        policy
-      else
-        action = _("Tag %{tag} was not on policy %{policy}") % {tag: data['tag'], policy: data['name']}
-        { :result => action }
-      end
+    if tag and policy.tags.include?(tag)
+      policy.remove_tag(tag)
+      policy
     else
       action = _("Tag %{tag} was not on policy %{policy}") % {tag: data['tag'], policy: data['name']}
       { :result => action }
