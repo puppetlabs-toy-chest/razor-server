0d4190db03973e9ff08b363473798f95ad4b822c
Scott McClellan <scott.mcclellan@puppetlabs.com>
2014-03-27 13:35:03 -0500

    Validate `remove_policy_tag` command.
    https://tickets.puppetlabs.com/browse/RAZOR-178

--- lib/razor/command/remove_policy_tag.rb
+++ lib/razor/command/remove_policy_tag.rb
@@ -1,27 +1,16 @@
 # -*- encoding: utf-8 -*-
 
 class Razor::Command::RemovePolicyTag < Razor::Command
-  attr 'name'
-  attr 'tag'
+  attr 'name', type: String, required: true, references: Razor::Data::Policy
+  attr 'tag',  type: String, required: true
 
   def run(request, data)
-    data['name'] or request.error 400,
-    :error => _("Supply policy name to which the tag is to be removed")
-    data['tag'] or request.error 400,
-    :error => _("Supply the name of the tag you which to remove")
-
-    policy = Razor::Data::Policy[:name => data['name']] or request.error 404,
-    :error => _("Policy %{name} does not exist") % {name: data['name']}
+    policy = Razor::Data::Policy[:name => data['name']]
     tag = Razor::Data::Tag[:name => data['tag']]
 
-    if tag
-      if policy.tags.include?(tag)
-        policy.remove_tag(tag)
-        policy
-      else
-        action = _("Tag %{tag} was not on policy %{policy}") % {tag: data['tag'], policy: data['name']}
-        { :result => action }
-      end
+    if tag and policy.tags.include?(tag)
+      policy.remove_tag(tag)
+      policy
     else
       action = _("Tag %{tag} was not on policy %{policy}") % {tag: data['tag'], policy: data['name']}
       { :result => action }
