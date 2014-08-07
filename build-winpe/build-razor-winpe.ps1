90c396611c30cec4c170d4454a91f46572414944
David Lutterkort <lutter@watzmann.net>
2014-08-07 13:08:49 -0700

    build-winpe: adapt to ADK 8.1
    Based on https://github.com/nanliu/razor-server/tree/win2012

--- build-winpe/build-razor-winpe.ps1
+++ build-winpe/build-razor-winpe.ps1
@@ -1,8 +1,8 @@
 # -*- powershell -*-
-# During development, this was run locally with:
-# powershell -executionpolicy bypass -file build-razor-winpe.ps1
+# Run this script as:
+#   powershell -executionpolicy bypass -file build-razor-winpe.ps1 -workdir C:\build-winpe
 #
-# For release we should sign the script, I guess?
+# Produce a WinPE image suitable for use with Razor
 
 # Parameters
 #   - workdir: where to create the WinPE image and intermediate files
@@ -26,7 +26,7 @@ if (-not (test-administrator)) {
 You must be running as administrator for this script to function.
 Unfortunately, we can't reasonable elevate privileges ourselves
 so you need to launch an administrator mode command shell and then
-re-run this script yourself.  Sorry.
+re-run this script yourself.
 "@
     exit 1
 }
@@ -47,14 +47,28 @@ $mount  = join-path $workdir "razor-winpe-mount"
 # versions of of the WinPE building tools or whatever.  These are
 # factored out mostly for my convenience, honestly.
 
-# Default install root for the ADK; since the installer database
-# does not contain custom paths, if any, this was installed to,
-# we are stuck with just defaulting and failing.
-$adk = @([Environment]::GetFolderPath('ProgramFilesX86'),
-         [Environment]::GetFolderPath('ProgramFiles')) |
-           % { join-path $_ 'Windows Kits\8.0\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64' } |
+# The ADK versions we can deal with. Different versions unfortunately
+# require slightly different handling
+$adkversions = @('8.1', '8.0')
+
+foreach ($adkversion in $adkversions) {
+    # Default install root for the ADK; since the installer database
+    # does not contain custom paths, if any, this was installed to,
+    # we are stuck with just defaulting and failing.
+    $adk = @([Environment]::GetFolderPath('ProgramFilesX86'),
+             [Environment]::GetFolderPath('ProgramFiles')) |
+           % { join-path $_ "Windows Kits\$adkversion\Assessment and Deployment Kit\Windows Preinstallation Environment\amd64" } |
            ? { test-path  $_ } |
            select-object -First 1
+    if ($adk -ne $null) { break }
+}
+if ($adk -eq $null) {
+    write-error @"
+We could not find the ADK - either it is not installed or not installed in
+the default location.
+"@
+    exit 1
+}
 
 # Path to the clean WinPE WIM file.
 $wim = join-path $adk "en-us\winpe.wim"
@@ -65,9 +79,9 @@ $packages = join-path $adk "WinPE_OCs"
 
 ########################################################################
 # ...and these are "constants" that are calculated from the above.
-write-host "Make sure our working and output directories exist."
+write-host "* Make sure our working and output directories exist."
 if (test-path -path $output) {
-    write-error "Output path $output already exists, aborting!"
+    write-error "Output path $output already exists, delete these folders and try again!"
     exit 1
 } else {
     new-item -type directory $output
@@ -78,35 +92,43 @@ if (-not(test-path -path $mount)) {
 }
 
 
-write-host "Copy the clean ADK WinPE image into our output area."
+write-host "* Copy the clean ADK WinPE image into our output area."
 copy-item $wim $output
 # update our wim location...
 $wim = join-path $output "winpe.wim"
 
 
 
-write-host "importing dism module"
 import-module dism
 
-write-host "mounting the wim image"
+write-host "* Mounting the wim image"
 mount-windowsimage -imagepath $wim -index 1 -path $mount -erroraction stop
 
-write-host "adding powershell, and dependencies, to the image"
+write-host "* Adding powershell, and dependencies, to the image"
 # This order is documented in http://technet.microsoft.com/library/hh824926.aspx
 # I guess you can't change it safely, so respect that.
-@('WinPE-WMI', 'WinPE-NetFX4', 'WinPE-Scripting', 'WinPE-PowerShell3') | foreach {
-    write-host "installing $_ to image"
+if ($adkversion -eq '8.0') {
+  $cabs = @('WinPE-WMI', 'WinPE-NetFX4', 'WinPE-Scripting', 'WinPE-PowerShell3')
+} elseif ($adkversion -eq '8.1') {
+  $cabs = @('WinPE-WMI', 'WinPE-NetFX', 'WinPE-Scripting', 'WinPE-PowerShell')
+} else {
+  write-error "We can not deal with ADK version $adkversion"
+  exit 1
+}
+
+foreach ($cab in $cabs ) {
+    write-host "** Installing $cab to image"
     # there must be a way to do this without a temporary variable
-    $pkg = join-path $packages "$_.cab"
+    $pkg = join-path $packages "$cab.cab"
     add-windowspackage -packagepath $pkg -path $mount
 }
 
-write-host "writing startup PowerShell script"
+write-host "* Writing startup PowerShell script"
 $file   = join-path $mount "razor-client.ps1"
 $client = join-path $cwd "razor-client.ps1"
 copy-item $client $file
 
-write-host "writing Windows\System32\startnet.cmd script"
+write-host "* Writing Windows\System32\startnet.cmd script"
 $file = join-path $mount "Windows\System32\startnet.cmd"
 set-content $file @'
 @echo off
@@ -117,7 +139,7 @@ powershell -executionpolicy bypass -noninteractive -file %SYSTEMDRIVE%\razor-cli
 echo dropping to a command shell now...
 '@
 
-write-host "unmounting and saving the wim image"
+write-host "* Unmounting and saving the wim image"
 dismount-windowsimage -save -path $mount -erroraction stop
 
-write-host "work is complete and the WIM should be ready to roll!"
+write-host "* Work is complete and the WIM should be ready to roll!"
