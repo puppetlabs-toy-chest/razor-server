98d18afdb9f4c560a3579ba5e813bc4f022a68f6
mamba <taageju4@sshgin.mgmt.sccloudpoc.net>
2015-10-14 13:57:02 +0200

    Add support for bypassing authentication, if request is from localhost.

---
+++ spec/app/local_no_auth.rb
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+require_relative '../spec_helper'
+require_relative '../../app'
+
+require 'json-schema'
+
+describe "local requests without authentication" do
+  include Rack::Test::Methods
+
+  let(:app) { Razor::App }
+
+  context "/api without auth" do
+
+    it "no auth for local requests if auth.allow_localhost is set" do
+      
+      Razor.config['auth.allow_localhost'] = true
+      get '/api/collections/repos', {}, {'REMOTE_ADDR' => '127.0.0.1'}
+
+      last_response.status.should == 200
+
+    end
+   
+    it "auth for non local requests if auth.allow_localhost is set" do
+      
+      Razor.config['auth.allow_localhost'] = true
+      get '/api/collections/repos', {}, {'REMOTE_ADDR' => '172.17.10.10'}
+
+      last_response.status.should == 401
+
+    end
+ 
+    it "auth for local requests if auth.allow_localhost is not set" do
+      
+      Razor.config['auth.allow_localhost'] = false
+      get '/api/collections/repos', {}, {'REMOTE_ADDR' => '127.0.0.1'}
+
+      last_response.status.should == 401
+
+    end
+
+    it "auth for non local requests if auth.allow_localhost is not set" do
+      
+      Razor.config['auth.allow_localhost'] = false
+      get '/api/collections/repos', {}, {'REMOTE_ADDR' => '172.17.10.10'}
+
+      last_response.status.should == 401
+
+    end
+
+  end
+
+end
