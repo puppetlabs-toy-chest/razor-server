b02c825bc5510f00822e6ade329eb56720bf7547
Scott McClellan <scott.mcclellan@puppetlabs.com>
2017-02-17 11:00:46 -0800

    Fix command execution when `auth.allow_localhost` is used
    Previously whenever a command execution was attempted, the
    `auth.allow_localhost` functionality would cause a failure, meaning
    non-authenticated localhost commands to return a 500 error. This commit results
    in the intended behavior for commands as well as collections.
    
    Fixes https://tickets.puppetlabs.com/browse/RAZOR-994

--- spec/app/local_no_auth.rb
+++ spec/app/local_no_auth.rb
@@ -9,44 +9,39 @@ describe "local requests without authentication" do
 
   let(:app) { Razor::App }
 
-  context "/api without auth" do
+  before :each do
+    header 'content-type', 'application/json'
+  end
 
+  context "/api without auth" do
     it "no auth for local requests if auth.allow_localhost is set" do
-      
       Razor.config['auth.allow_localhost'] = true
       get '/api/collections/repos', {}, {'REMOTE_ADDR' => '127.0.0.1'}
-
       last_response.status.should == 200
 
+      post '/api/commands/create-broker', {'name' => 'abc', 'broker-type' => 'puppet'}.to_json
+      last_response.status.should == 202
     end
    
     it "auth for non local requests if auth.allow_localhost is set" do
-      
       Razor.config['auth.allow_localhost'] = true
       get '/api/collections/repos', {}, {'REMOTE_ADDR' => '172.17.10.10'}
 
       last_response.status.should == 401
-
     end
  
     it "auth for local requests if auth.allow_localhost is not set" do
-      
       Razor.config['auth.allow_localhost'] = false
       get '/api/collections/repos', {}, {'REMOTE_ADDR' => '127.0.0.1'}
 
       last_response.status.should == 401
-
     end
 
     it "auth for non local requests if auth.allow_localhost is not set" do
-      
       Razor.config['auth.allow_localhost'] = false
       get '/api/collections/repos', {}, {'REMOTE_ADDR' => '172.17.10.10'}
 
       last_response.status.should == 401
-
     end
-
   end
-
 end
