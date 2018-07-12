66ed017293a7fd8a60ed8362efddc0e454a466cd
Scott McClellan <scott.mcclellan@puppetlabs.com>
2018-07-12 02:30:02 -0500

    (maint) Update acceptance tests for puppet6
    - The `puppet master` subcommand doesn't exist anymore.
    - The repo structure has moved to `puppet6` rather than `PC1`.

--- acceptance/suites/steps/install_pe.rb
+++ acceptance/suites/steps/install_pe.rb
@@ -1,3 +1,6 @@
 step "Install PE"
-  install_pe
+  # TODO `puppet_collection` argument must be supplied because beaker is still
+  # looking for PC1 for PE 2018.2.
+  # install_pe
+  install_pe_on(hosts, options.merge(puppet_collection: 'puppet6'))
 
